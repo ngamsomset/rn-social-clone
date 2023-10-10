e189454377cfadde837e8e32ed42a95eacb1d7fe
ngamsomset <setthasiri.ngm@gmail.com>
2023-10-10 11:08:27 +1100

    passing props

--- src/components/FeedPost.js
+++ src/components/FeedPost.js
@@ -7,23 +7,7 @@ import {
 import { StyleSheet, Text, View, Image } from 'react-native';
 import LikeImage from '../../assets/images/like.png';
 
-const post = {
-  id: 'p1',
-  createdAt: '19 m',
-  User: {
-    id: 'u1',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
-    name: 'Brian Ngamsom',
-  },
-  description:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  numberOfLikes: 11,
-  numberOfShares: 1,
-};
-
-const FeedPost = () => {
+const FeedPost = ({ post }) => {
   return (
     <View style={styles.container}>
       <View>
