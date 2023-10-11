9c84426576730910b8d1fd80160c76e11725a970
ngamsomset <setthasiri.ngm@gmail.com>
2023-10-12 10:20:45 +1100

    add profile buttons

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,7 +5,15 @@ import {
   Dimensions,
   Text,
   Image,
+  Pressable,
 } from 'react-native';
+import {
+  AntDesign,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Ionicons,
+  Entypo,
+} from '@expo/vector-icons';
 import user from '../../assets/data/user.json';
 
 const dummy_img =
@@ -14,17 +22,59 @@ const dummy_img =
 const bg = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg';
 const profilePictureWidth = Dimensions.get('window').width * 0.4;
 
+function signOut() {}
+
 const ProfileScreen = () => {
   return (
     <View style={styles.container}>
       <Image source={{ uri: bg }} style={styles.bg} />
       <Image source={{ uri: user?.image || dummy_img }} style={styles.image} />
       <Text style={styles.userName}>{user.name}</Text>
+      {/* button container */}
+      <View style={styles.buttonContainer}>
+        <Pressable style={[styles.button, { backgroundColor: 'royalblue' }]}>
+          <AntDesign name="pluscircle" size={16} color="white" />
+          <Text style={[styles.buttonText, { color: 'white' }]}>
+            Add to story
+          </Text>
+        </Pressable>
+        <Pressable style={styles.button}>
+          <MaterialCommunityIcons name="pencil" size={16} color="black" />
+          <Text style={styles.buttonText}>Edit Profile</Text>
+        </Pressable>
+        <Pressable
+          onPress={signOut}
+          style={[styles.button, { flex: 0, width: 50 }]}
+        >
+          <MaterialIcons name="logout" size={16} color="black" />
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    borderColor: 'lightgray',
+    paddingVertical: 5,
+  },
+  button: {
+    flexDirection: 'row',
+    padding: 7,
+    borderRadius: 5,
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'gainsboro',
+    alignSelf: 'stretch',
+    margin: 5,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontWeight: '500',
+    marginHorizontal: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
